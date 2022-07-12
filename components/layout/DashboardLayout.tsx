68faac699679c74f57246a2e4d4fb9e070f87667
simon <simon.weckler@mnet-online.de>
2022-07-12 20:39:45 +0200

    feat: added landing page

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -1,14 +1,33 @@
 import { PropsWithChildren } from "react";
 import Container from "./Container";
-import DashboardNav from "./DashboardNav";
+import { usePostRequest } from "$lib/request/clientRequest";
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import Nav from "./Nav";
 
 export default function DashboardLayout({
 	children,
 }: PropsWithChildren<Record<string, never>>) {
+	const { loading, error, send, data } = usePostRequest("/api/auth/logout");
+	const router = useRouter();
+
+	if (!loading && !error && data) {
+		router.push("/");
+	}
+
 	return (
 		<>
 			<Container mb={4} borderBottomWidth="2px">
-				<DashboardNav />
+				<Nav>
+					<Button
+						onClick={() => {
+							send();
+						}}
+						colorScheme="red"
+						variant="outline">
+						Logout
+					</Button>
+				</Nav>
 			</Container>
 			<Container>{children}</Container>
 		</>
